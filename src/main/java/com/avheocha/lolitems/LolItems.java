2f0c9a23bf20e6624d4fbf48eaf7797dcf2efb7c
Avery <avheocha@gmail.com>
2025-05-23 17:07:17 +0100

    Added Dagger

--- src/main/java/com/avheocha/lolitems/LolItems.java
+++ src/main/java/com/avheocha/lolitems/LolItems.java
@@ -1,6 +1,8 @@
 package com.avheocha.lolitems;
 
+import com.avheocha.lolitems.item.ModItems;
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -34,6 +36,8 @@ public class LolItems
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -47,7 +51,9 @@ public class LolItems
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.DAGGER);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/com/avheocha/lolitems/item/ModItems.java
@@ -0,0 +1,21 @@
+package com.avheocha.lolitems.item;
+
+import com.avheocha.lolitems.LolItems;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, LolItems.MOD_ID);
+
+    public static final RegistryObject<Item> DAGGER = ITEMS.register("dagger", () -> new Item(new Item.Properties()));
+
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+}
